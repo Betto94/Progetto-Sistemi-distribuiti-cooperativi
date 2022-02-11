53352204de6bdeb92f18ce7a3cad1038358eb7fa
MissFoxy394 <missfoxy394@pop-os.localdomain>
2022-02-11 15:36:43 +0100

    Aggiunti commenti file GestoreGenerale

--- contracts/GestoreGenerale.sol
+++ contracts/GestoreGenerale.sol
@@ -10,7 +10,6 @@ import "./GestioneRuoli.sol";
 */
 contract GestoreGenerale is GestioneRuoli{
 
-    
     //mapping(bytes32 => mapping(bytes32 => Carta)) public listaCarte_clienti; //
     mapping(bytes32 => Carta) public carte;
 
@@ -19,6 +18,10 @@ contract GestoreGenerale is GestioneRuoli{
         bytes32 id_cliente
     );
 
+    /**
+    * @dev Funzione che crea una nuova carta per un cliente
+    * @param c cliente per il quale va creata la carta
+    */
     function addCarta(Cliente memory c) public onlyFunzionarioOrOwner returns(bytes32) {
         uint date = block.timestamp;
         bytes32 ID = keccak256(abi.encodePacked(c.nome, c.cognome, c.dataDiNascita, date));
@@ -29,47 +32,78 @@ contract GestoreGenerale is GestioneRuoli{
         return ID;
     }
 
+    /**
+    * @dev Funzione che aggiunge nuovi punti ad una carta
+    * @param id_carta codice univoco della carta
+    * @param numeroPunti numero dei punti da aggiungere alla carta
+    */
     function addPunti(bytes32 id_carta, uint numeroPunti) public onlyFunzionarioOrOwner{
         carte[id_carta].punti += numeroPunti;
         //listaCarte_clienti[id_cliente][id_carta].punti += numeroPunti;
     }
 
+
+    /**
+    * @dev Funzione che decrementa i punti ad una carta
+    * @param id_carta codice univoco della carta
+    * @param numeroPunti numero dei punti da decrementare dalla carta
+    */
     function removePunti(bytes32 id_carta, uint numeroPunti) public onlyFunzionarioOrOwner{
         //listaCarte_clienti[id_cliente][id_carta].punti -= numeroPunti;
         carte[id_carta].punti -= numeroPunti;
     }
 
     /**
-    *Prende in input un id_carta, restituisce i punti
+    * @dev Funzione che permette di leggere l'ammontare di punti di una carta
+    * @param id_carta codice univoco della carta
     */
     function getPunti(bytes32 id_carta) public view returns(uint){
         return carte[id_carta].punti;
         //return listaCarte_clienti[id_cliente][id_carta].punti;
     }
 
-
-    //Prende in input una carstringa e la inserisce nei dati di un cliente. Utile qualora si dovesse inserire notazioni su un cliente che ha avuto problemi con la carta o il negozio
-    function addNote(bytes32 id, string memory descrizione) public onlyFunzionarioOrOwner{
-        clienti[id].note = descrizione;
+    /**
+    * @dev Funzione che inserisce una notazione nei dati di un cliente, 
+    * utile qualora si dovesse inserire notazioni su un cliente che ha avuto problemi con la carta o il negozio
+    * @param id codice univoco del cliente
+    * @param notazione la notazione o descrizione che si vuole inserire su un cliente
+    */
+    function addNote(bytes32 id, string memory notazione) public onlyFunzionarioOrOwner{
+        clienti[id].note = notazione;
     }
 
-    //Prende in input una carta e ne azzera i punti
+    /**
+    * @dev Funzione che azzera i punti di una carta
+    * @param id codice univoco della carta
+    */
     function azzeraPunti(bytes32 id) public onlyFunzionarioOrOwner{
         carte[id].punti = 0;
     }
 
-    //Prende in input due carte e trasferisce i punti da una alla seconda. (Es: carta smagnetizzata, ne occorre fare una nuova)
+    /**
+    * @dev Funzione che sposta i punti da una specifica carta ad un'altra. 
+    * Utile qualora serva la sostituzione di una carta (Es: carta smagnetizzata, ne occorre fare una nuova)
+    * oppure un cliente con più carte volesse unirle
+    * @param id_from codice univoco della carta dalla quale prendere i punti
+    * @param id_destinatario codice univoco della carta alla quale saranno aggiunti i punti
+    */
     function movePunti(bytes32 id_from, bytes32 id_destinatario) public onlyFunzionarioOrOwner{
         uint ammontare = carte[id_from].punti;
         carte[id_destinatario].punti += ammontare;
     }
 
-    //Dato in input un id di una carta, la cancella
+    /**
+    * @dev Funzione che elimina una carta
+    * @param id codice univoco della carta
+    */
     function deleteCarta(bytes32 id) public onlyFunzionarioOrOwner{
         delete carte[id];
     }
 
-    //Dato in input un id di una carta, la cancella
+    /**
+    * @dev Funzione che elimina un cliente
+    * @param id_cliente codice univoco del cliente
+    */
     function deleteCliente(bytes32 id_cliente) public onlyFunzionarioOrOwner{
         delete clienti[id_cliente];
     }
